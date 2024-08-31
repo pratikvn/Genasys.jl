fc0681583419f0a6a2c124426c69a433c65e91ae
Documenter.jl <documenter@juliadocs.github.io>
2024-08-31 21:00:14 +0000

    build based on aba2248

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Genasys","category":"page"},{"location":"#Genasys","page":"Home","title":"Genasys","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Genasys.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Genasys]","category":"page"}]
+}
