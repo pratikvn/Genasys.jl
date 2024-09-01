0875ee6c4cd31a842e395334aadb7f881b931747
Pratik Nayak <pratikvn@protonmail.com>
2024-09-01 21:17:36 +0200

    [test] add stencil tests

--- src/Genasys.jl
+++ src/Genasys.jl
@@ -6,5 +6,6 @@ import SparseArrays
 # Write your package code here.
 include("Laplace.jl")
 include("MtxIO.jl")
+include("Stencil.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,14 @@ using Test
     @test Genasys.laplacian(2, 2) == sparse([1, 2, 3, 1, 2, 4, 1, 3, 4, 2, 3, 4], [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4], [4.0, -1.0, -1.0, -1.0, 4.0, -1.0, -1.0, 4.0, -1.0, -1.0, -1.0, 4.0], 4, 4)
 end
 
+@testset "Stencil tests" begin
+    @test Genasys.stencilmat(1, [-1 2 -1]) == sparse([1], [1], [2])
+    @test Genasys.stencilmat(3, [-1 2 -1]) == sparse([1, 2, 1, 2, 3, 2, 3], [1, 1, 2, 2, 2, 3, 3], [2, -1, -1, 2, -1, -1, 2])
+end
+
 @testset "MtxIO tests" begin
     mat = sparse([1, 2], [1, 1], [2.5, 4.5])
     @test Genasys.read("data/testmat.mtx") == sparse([1], [1], [2.5])
-    Genasys.write("data/testout.mtx", mat)
-    @test Genasys.read("data/testout.mtx") == sparse([1, 2], [1, 1], [2.5, 4.5])
+    Genasys.write("tmp/testout.mtx", mat)
+    @test Genasys.read("tmp/testout.mtx") == sparse([1, 2], [1, 1], [2.5, 4.5])
 end
