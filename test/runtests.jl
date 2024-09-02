46aa6e9208aff2a6e86435c6a55d2d3aca16132e
Pratik Nayak <pratikvn@protonmail.com>
2024-09-02 13:46:01 +0200

    [tests] update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,12 @@ using LinearAlgebra
 using SparseArrays
 using Test
 
+@testset "Multi-dim Kronecker gen tests" begin
+    @test Genasys.grid3Dkron(sparse(I, 3, 3), sparse(I, 2, 2), sparse(I, 3, 3)) == sparse(3 * I, 18, 18)
+    @test Genasys.grid2Dkron(sparse(I, 3, 3), sparse(I, 2, 2)) == sparse(2 * I, 6, 6)
+    @test Genasys.grid2Dkron(sparse(2.5 * I, 3, 3), sparse(I, 2, 2)) == sparse(3.5 * I, 6, 6)
+end
+
 @testset "Stencil tests" begin
     @test Genasys.stencilmat([-1 2 -1], 0) == sparse([], [], [])
     @test Genasys.stencilmat([-1 2 -1], 0, 0) == sparse([], [], [])
